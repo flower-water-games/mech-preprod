91b9de957dce0f2cdd0057a8bc2c7580074ed80c
squiffel <qytiswork@gmail.com>
2024-07-11 09:02:53 -0400

    leg stuff

--- scenes/GameScene/Components/MovementTracking/MovementTracking.gd
+++ scenes/GameScene/Components/MovementTracking/MovementTracking.gd
@@ -4,36 +4,49 @@ extends Node2D
 
 @onready var line_2d : Line2D = $Line2D
 @onready var foot : Sprite2D = $Foot
+@onready var target_position_sprite : Sprite2D = $TargetPosition
 
-var track_position : Vector2 = Vector2.ZERO
+var previous_position : Vector2 = Vector2.ZERO
+
+var foot_position : Vector2 = Vector2.ZERO
 var walk_distance : float = 64.0
+
+var target_position : Vector2 = Vector2.ZERO
 
 func _ready():
 	update_foot_offset.call_deferred()
 
 func update_foot_offset():
 	update_position()
+	previous_position = global_position
 	if offset_foot:
-		track_position -= Vector2(walk_distance * 0.5, 0)
+		foot_position -= Vector2(walk_distance * 0.5, 0)
 
 func update_position():
-	track_position = global_position
+	foot_position = global_position
 
 func _process(delta):
-	# Move foot position
-	var relative_position = track_position - global_position
-	var distance_from_previous_position = global_position.distance_to(track_position)
+	# Moving direction
+	var moving_direction = previous_position.direction_to(global_position)
+	var target_position = moving_direction * walk_distance
+	if target_position != Vector2.ZERO:
+		target_position_sprite.position = target_position
+
+	# Update tracking if you move past the distance
+	if foot_position.distance_to(global_position) >= walk_distance:
+		foot_position = target_position_sprite.global_position
 	
+	# Move foot position
+	var relative_foot_position = foot_position - global_position
+
 	# Update position
 	line_2d.clear_points()
 	line_2d.add_point(Vector2.ZERO)
-	line_2d.add_point(relative_position)
-
-	# Update tracking if you move past the distance
-	if distance_from_previous_position > walk_distance:
-		update_position()
+	line_2d.add_point(relative_foot_position)
 	
-	# Foot lerps to new position
-	foot.position = relative_position
-	#foot.position.x = lerpf(foot.position.x, relative_position.x, 0.1)
-	#foot.position.y = lerpf(foot.position.y, relative_position.y, 0.1)
+	# Smooth animate the jump between positions
+	foot.global_position.x = lerpf(foot.global_position.x, foot_position.x, 0.1)
+	foot.global_position.y = lerpf(foot.global_position.y, foot_position.y, 0.1)
+
+	# Store previous position
+	previous_position = global_position
