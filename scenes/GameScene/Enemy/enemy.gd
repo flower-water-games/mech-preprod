f94a4bffa31865e6feefe50aa67c021383f22992
gloriousclover <51941980+gloriousclover@users.noreply.github.com>
2024-07-14 15:19:26 -0600

    Enemy changes
    Added death animation, refactored functions to match scope, new cleanup approach using position, removed enemy damage for upcoming melee changes

--- scenes/GameScene/Enemy/enemy.gd
+++ scenes/GameScene/Enemy/enemy.gd
@@ -13,16 +13,13 @@ enum MovementType {
 # btw, totally reasonable to refactor to set these variables in enemyfactory by code, but scenes work fine for this for now
 @export var custom_velocity := Vector2(-125, 0)
 @export var shoots := false
-@export var on_hit_damage = 5
+@export var on_hit_damage = 0.0
 @export var movement_type : MovementType = MovementType.VERTICAL_SPLINE
 @export var spline_amplitude := 100.0  # Maximum vertical displacement
 @export var spline_frequency := 3.5  # How fast the enemy moves up and down
 @export var spline_randomized := false
 @export var score = 50 # when enemy dies, how much score
 
-# after this value of time has elapsed, kill this entity
-var lifetime = 10.0
-var death_timer : Timer
 signal enemy_died
 
 var time_passed = 0.9
@@ -39,29 +36,27 @@ enum ACTOR_STATE {
 
 func _ready() -> void:
 	_init_properties()
-	_init_cleanup_timer()
 
 func _init_properties():
 	_actor_state = ACTOR_STATE.run
-	health.died.connect(enemy_die)
+
+	health.died.connect(_enemy_die)
+	$SpriteSheet/AnimationPlayer.animation_finished.connect(_deathanim_end)
+
 	velocity = custom_velocity
 	if (spline_randomized):
 		initial_y_position = global_position.y + randf_range(-350, 350)
 	else:
 		initial_y_position = global_position.y
 
-# Used to clean up enemies past the screen, may replace with something else later
-func _init_cleanup_timer():
-
-	death_timer = Timer.new()
-	death_timer.set_one_shot(true)
-	death_timer.set_wait_time(lifetime)
-	death_timer.timeout.connect(enemy_destroy)
-	add_child(death_timer)
-	death_timer.start()
-
 func _process(delta: float) -> void:
 	time_passed += delta
+	_process_animations()
+	_check_cleanup()
+
+func _check_cleanup():
+	if (global_position.x <= -100):
+		_enemy_destroy()
 
 func _process_animations():
 	var AP = $SpriteSheet/AnimationPlayer
@@ -69,7 +64,7 @@ func _process_animations():
 		ACTOR_STATE.run:
 			AP.play("run")
 		ACTOR_STATE.dead:
-			AP.play("dead")
+			AP.play("death")
 
 func _physics_process(delta: float) -> void:
 	_process_movement(delta)
@@ -77,19 +72,19 @@ func _physics_process(delta: float) -> void:
 func _process_movement(delta):
 	match movement_type:
 		MovementType.LINEAR:
-			move_linear(delta)
+			_move_linear(delta)
 		MovementType.VERTICAL_SPLINE:
-			move_vertical_spline(delta)
+			_move_vertical_spline(delta)
 		MovementType.STOPPED:
 			velocity = Vector2(0,0)
 			move_and_slide()
 
-func move_linear(delta: float) -> void:
+func _move_linear(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		_handle_hit_collision(collision)
 
-func move_vertical_spline(delta: float) -> void:
+func _move_vertical_spline(delta: float) -> void:
 	var horizontal_movement = velocity.x * delta
 	var vertical_movement = sin(time_passed * spline_frequency) * spline_amplitude
 	var target_position = Vector2(global_position.x + horizontal_movement, initial_y_position + vertical_movement)
@@ -102,12 +97,16 @@ func _handle_hit_collision(col : KinematicCollision2D) -> void:
 	var collider : Node2D = col.get_collider()
 	if collider.is_in_group("Player"):
 		collider.health.add_or_subtract_health_by_value(-on_hit_damage)
-		enemy_die()
 
-func enemy_destroy() -> void:
+func _enemy_destroy() -> void:
 	queue_free()
 
-func enemy_die() -> void:
+func _deathanim_end(animation_name):
+	if animation_name == "death":
+		_enemy_destroy()
+
+func _enemy_die() -> void:
 	enemy_died.emit()
 	movement_type = MovementType.STOPPED
-	queue_free()
+	_actor_state = ACTOR_STATE.dead
+	collision_layer = 0
