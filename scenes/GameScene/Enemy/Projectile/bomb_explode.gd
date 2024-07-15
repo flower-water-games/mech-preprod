c72acc458cf84c76aa5f4873e516f698fa35bd46
gloriousclover <51941980+gloriousclover@users.noreply.github.com>
2024-07-14 23:55:56 -0600

    Remove obsolete testing

--- scenes/GameScene/Enemy/Projectile/bomb_explode.gd
+++ scenes/GameScene/Enemy/Projectile/bomb_explode.gd
@@ -8,20 +8,18 @@ func _ready():
 	AP.play("play")
 	AP.animation_finished.connect(_playanim_end)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if is_colliding():
 		_process_collision()
 
 func _process_collision():
 	# Assumption, only ever one player object
-	var collision_count = get_collision_count()
 	var collider = get_collider(0)
 	if collider.is_in_group("Player"):
 		collision_mask = 0
 		var dist = global_position.distance_to(collider.global_position)
 		var percent_damage = 1 - (dist/_collision_max_radius)
 		var calc_damage = clamp(_max_damage * percent_damage, 0, _max_damage)
-		print("Calcualted damage: " + str(calc_damage))
 		collider.health.add_or_subtract_health_by_value(-calc_damage)
 
 func _playanim_end(animation_name):
