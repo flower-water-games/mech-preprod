7685901c654f22ac23569bcd0f6adbcdcd60bd16
squiffel <qytiswork@gmail.com>
2024-07-15 21:21:17 -0400

    Add leg code

---
+++ scenes/GameScene/Player/Mech/MovementTracking/Body.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var top_left_leg : Node2D
+@export var top_right_leg : Node2D
+@export var bottom_left_leg : Node2D
+@export var bottom_right_leg : Node2D
+
+func _ready():
+	pass
+
+func _process(delta):
+	var target_center = get_center_of_positions(top_left_leg.foot.global_position, top_right_leg.foot.global_position, bottom_left_leg.foot.global_position, bottom_right_leg.foot.global_position)
+	global_position.x = lerpf(global_position.x, target_center.x, 0.05)
+	global_position.y = lerpf(global_position.y, target_center.y, 0.05)
+
+func get_center_of_positions(pos1, pos2, pos3, pos4):
+	var center_x = (pos1.x + pos2.x + pos3.x + pos4.x) / 4
+	var center_y = (pos1.y + pos2.y + pos3.y + pos4.y) / 4
+	return Vector2(center_x, center_y)
