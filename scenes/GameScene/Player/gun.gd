9b1fef6b9829ec2d33b600a40105611b4e6c72ba
gloriousclover <51941980+gloriousclover@users.noreply.github.com>
2024-07-16 23:45:32 -0600

    Double Guns

--- scenes/GameScene/Player/gun.gd
+++ scenes/GameScene/Player/gun.gd
@@ -7,6 +7,7 @@ class_name Gun
 @export var bullet_scene : PackedScene
 @export var cursor: Cursor
 @export var bulletspawn: Node2D
+@export var gunpos: Sprite2D
 
 var current_fire_rate := 0.2
 var can_fire := true
@@ -27,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("shoot"):
 		if can_fire:
 			shoot()
-			position = position.direction_to(cursor.position) * -4.0
+			gunpos.position = gunpos.position.direction_to(cursor.position) * -8.0
 			can_fire = false
 			fire_rate_timer = current_fire_rate
 
@@ -54,11 +55,11 @@ func _physics_process(delta: float) -> void:
 	_rotate_turret()
 
 	# Return to position
-	position.x = lerpf(position.x, 0, 0.2)
-	position.y = lerpf(position.y, 0, 0.2)
+	gunpos.position.x = lerpf(gunpos.position.x, 0, 0.2)
+	gunpos.position.y = lerpf(gunpos.position.y, 0, 0.2)
 
 func _rotate_turret():
-	rotation = position.angle_to_point(cursor.position)
+	gunpos.rotation = gunpos.position.angle_to_point(cursor.position)
 
 func is_currently_shooting() -> bool:
 	return is_shooting
